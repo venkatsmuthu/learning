f86a639021e625b1a8710aa6e9fecfa333e0151c
venkatsmuthu <venkat.smuthu@gmail.com>
2025-02-13 18:15:43 +0300

    Add files via upload

---
+++ Python/py2.py
@@ -0,0 +1,26 @@
+#This is python program 
+print ('Welcome to Python')
+print('This is Python'+" Language")
+print('Displaying number 10 - ' , 10)
+print('test with semicolon');
+print(10,20,30)
+if 10 < 20:
+    print('10 is less than 20')
+if 10 < 5:
+    print('10 is less than 5')
+else:
+    print('10 is greater than 5')
+print('printing Numbers 0-4')
+for i in range(5):
+    print (i)
+print('printing Numbers 0-4')
+for i in range(0,5):
+    print(i)
+print('printing Numbers 0-4')
+a=0
+while a<5:
+    print(a)
+    a+=1
+a=""" This is multi-line string.
+it's a feature of "python" """
+print(a)
---
+++ Python/py3_string.py
@@ -0,0 +1,21 @@
+a="Welcome to python"
+print(a)
+print(a[1])
+print(a[3:7])
+print(a[:7])
+print(a[11:])
+print(a[-6:])
+print(a[-6:-1])
+a="  WELCOME to Python"
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.strip())
+print(a.replace("Python","Java"))
+print("Hello,World".split(','))
+a=10
+b=20
+c=a*b
+print(f"a*b is {10*20}")
+print(f'a*b is {a*b}')
+print(f'a*b is {c:.2f}')
